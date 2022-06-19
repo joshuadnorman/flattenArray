374bcc7a6c15248c91bcfeb30551390091d6b9e3
Joshua Norman <joshuadnorman@gmail.com>
2022-06-19 17:48:15 +0200

    Added recursive flatten function and index.js to add some custom arrays seperate from tests

--- flatten.js
+++ flatten.js
@@ -1,5 +1,19 @@
-function flatten(array){
-    //flatten array 
+function flatten(arr){
+    if (!Array.isArray(arr)){
+        return 
+    }
+
+    let newArray = [];
+
+    arr.forEach(item => {
+       if (Array.isArray(item)){
+        newArray = newArray.concat(flatten(item))
+       } else {
+        newArray.push(item)
+       }
+    });
+
+    return newArray
 }
 
 module.exports = flatten
---
+++ index.js
@@ -0,0 +1,6 @@
+//Created this to add and run some manual tests
+
+const flatten = require('./flatten')
+
+//Example provided from assessment desciption
+console.log(flatten([ 1, [ 2, [ 3 ] ], 4 ]))
